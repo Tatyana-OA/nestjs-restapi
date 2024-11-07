1523f1c1bc944f837246dda431575480f5973c4c
TAsenova <tasenova@mentormate.com>
2024-11-07 17:39:22 +0200

    Apply logger globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //Apply logger globally
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(MyLoggerService));
   // Allowing all requests; if configured, we can allow specific urls that can make requests
   app.enableCors();
   app.setGlobalPrefix('api');
